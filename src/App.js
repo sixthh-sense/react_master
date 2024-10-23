ef6f0d5d0711b58b4f4a376cda44d6e44fb7131e
haein_sung <blue6zombie6@gmail.com>
2024-10-23 17:30:39 +0900

    #41 synthetic event 추가 설명 - test를 누를 때랑 Get Data를 누를 때랑 innerHTML가 서로 다르게 출력됨

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import "./css/styles.css";
 
 export default function App() {
 
-    // e: synthetic event (wrapper around the native browser event object && passed to every function automatically)
+    // e: synthetic event (wrapper around the native browser event object && passed to every function automatically) // doesn't need to be passed
     const getData = (e) => {
         console.log(e.target.innerHTML);
         // e.target: button 요소가 나옴(Content of HTML)
-        // e.target.innerHTML: 요소 사이에 있는 내용(현재 같은 경우 Get Data)
+        // e.target.innerHTML: 요소 사이에 있는 내용
     }
 
     return (
         <div className="App">
-            <button onClick={getData}>Get Data</button>
+            <div onClick={getData}>
+                <h1>Test</h1>
+                Get Data
+            </div>
         </div>
     )
 }
