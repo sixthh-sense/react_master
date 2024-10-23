3417cd7d88777e48881222348e324114e15f4312
haein_sung <blue6zombie6@gmail.com>
2024-10-23 14:48:57 +0900

    #11 parameter passing - onClick에 arrow function을 안 쓰면 아주 재미있는(?) 오류를 볼 수 있음

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React, {useState} from "react";
 import "./css/styles.css";
 
-// lazy initialization
-function initialState() {
-    console.log("Expensive process: " + Date.now());
-    return 0;
-}
-
 export default function App() {
 
-    const [cnt, setCount] = useState(() => initialState());
-    // on every re-render the useState(0 & initialState() is executed
+    const [i, setCount] = useState(0);
+
+    const changeCounter = (op) => {
+        if (op === '+') {
+            setCount(i + 1);
+        } else if (op === '-') {
+            setCount(i - 1);
+        }
+    }
+
     return (
         <div className="App">
-            <h1>{cnt}</h1>
-            <button onClick={() => setCount(cnt + 1)}>Increment</button>
+            <h1>{i}</h1>
+            <button onClick={() => changeCounter('+')}>Increment</button>
+            <button onClick={() => changeCounter('-')}>Decrement</button>
         </div>
     );
 }
