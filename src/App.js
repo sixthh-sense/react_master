ea65aecbc1e52297d51cc5d220f7e0f383db2d3b
haein_sung <blue6zombie6@gmail.com>
2024-10-23 16:17:28 +0900

    #28

--- src/App.js
+++ src/App.js
@@ -2,30 +2,18 @@ import React, {useState} from "react";
 import "./css/styles.css";
 
 export default function App() {
-
-    const [i, setCount] = useState(0);
-
-    const addValue = () => {
-        setCount(i + 1);
-    }
-
-    const decrementValue = () => {
-        setCount(i - 1);
-    }
-
-    const changeCounter = (op) => {
-        if (op === '+') {
-            setCount(i + 1);
-        } else if (op === '-') {
-            setCount(i - 1);
-        }
-    }
+    const temp = ["Apple", "Banana", "Grapes", "Orange"];
+    const [products, setProducts] = useState(temp);
 
     return (
         <div className="App">
-            <h1>{i}</h1>
-            <button onClick={addValue}>Increment</button>
-            <button onClick={decrementValue}>Decrement</button>
+            {
+                products.map((product) => {
+                    return (
+                        <li>{product}</li>
+                    )
+                })
+            }
         </div>
     );
 }
