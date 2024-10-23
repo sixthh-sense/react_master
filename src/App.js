2d90255060d7403248fc58c289cb112ddfd19bc1
haein_sung <blue6zombie6@gmail.com>
2024-10-23 17:56:13 +0900

    #42 input창에 적힌 대로 특정 요소의 값 다르게 보이게 하기

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React from "react";
+import React , {useState} from "react";
 import "./css/styles.css";
 
 export default function App() {
 
+    const [header, setHeader] = useState('Default');
+
     // e: synthetic event (wrapper around the native browser event object && passed to every function automatically) // doesn't need to be passed
     const getData = (e) => {
-        console.log(e.target.innerHTML);
-        // e.target: button 요소가 나옴(Content of HTML)
-        // e.target.innerHTML: 요소 사이에 있는 내용
+        setHeader(e.target.value);
+        // 한 자씩 칠 때마다 적은 값이 콘솔창에 찍힘
+        console.log(e.target.value);
     }
 
     return (
         <div className="App">
-            <div onClick={getData}>
-                <h1>Test</h1>
-                Get Data
-            </div>
+            <h1>Test</h1>
+            <input onChange={getData} type="text"/>
         </div>
     )
 }
