724b83ab63b130959148e612d4373ca6afb2a071
haein_sung <blue6zombie6@gmail.com>
2024-10-23 17:28:02 +0900

    #41 synthetic event

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import "./css/styles.css";
 
 export default function App() {
 
-    const getData = () => {
-        console.log("Function called");
+    // e: synthetic event (wrapper around the native browser event object && passed to every function automatically)
+    const getData = (e) => {
+        console.log(e.target.innerHTML);
+        // e.target: button 요소가 나옴(Content of HTML)
+        // e.target.innerHTML: 요소 사이에 있는 내용(현재 같은 경우 Get Data)
     }
 
     return (
         <div className="App">
-            <button onClick={() => getData()}>Get Data</button>
+            <button onClick={getData}>Get Data</button>
         </div>
     )
 }
