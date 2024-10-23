26d770a6f16b57ae813d08a179956c9a75a2841c
haein_sung <blue6zombie6@gmail.com>
2024-10-23 16:28:57 +0900

    #30 - 그냥 index는 uniqueness가 보장되지 않지만 db에서 가져온 unique column이라면 괜찮겠지

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import React, {useState} from "react";
 import "./css/styles.css";
 
 export default function App() {
-    const temp = ["Apple", "Banana", "Grapes", "Orange"];
+    const temp = [
+        {id:1, pName: 'Apple', price:'$10'},
+        {id:2, pName: 'Banana', price:'$5'},
+        {id:3, pName: 'Grapes', price:'$7'},
+        {id:4, pName: 'Orange', price:'$8'}
+    ];
     const [products, setProducts] = useState(temp);
 
     // whenever using the map() method, make sure the key attribute is "unique"
+    // BEWARE: index is NOT a stable identity for every item
     return (
         <div className="App">
             {
                 products.map((product, i) => {
                     return (
-                        <li key={i.toString()}>{product}</li>
+                        <li key={product.id.toString()}>{product.pName}</li>
                     )
                 })
             }
